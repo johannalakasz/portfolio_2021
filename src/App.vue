40220e4d0e5be28371ef057414a09a3fff562dc8
johannalakasz <johannalakasz@gmail.com>
2021-05-03 11:23:45 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,201 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- <div id="nav-container"> -->
+      <div id="nav">
+       <!-- <router-link to="/">Home</router-link> | -->
+        <router-link to="/contact">Contact</router-link>
+      </div>
+      <router-view/>
+   <!-- </div> -->
+
+    <div id="page-bg"></div>
+
+    <div class="animation-wrapper">
+      <div class="particle particle-1"></div>
+      <div class="particle particle-2"></div>
+      <div class="particle particle-3"></div>
+      <div class="particle particle-4"></div>
     </div>
-    <router-view/>
+
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+//  text-align: center;
   color: #2c3e50;
+
+}
+* {
+  background: #1e1e1e;
+  margin: 0;
+  padding: 0;
 }
 
+#port-title {
+  font-family: 'Bebas Neue';
+  font-size: 3vw;
+  letter-spacing: 10px;
+//  margin-left: 1.5vw;
+  color: #f2f2f2;
+}
+
+h1 {
+  font-family: 'Bebas Neue';
+  font-size: 2vw;
+  letter-spacing: 0.5vw;
+ // margin-left: 2vw;
+  padding-top: 1vh;
+  color: #f2f2f2;
+  
+}
+
+h2 {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+  margin-left: 7vw;
+  margin-bottom: 1vh;
+  color: #f4f4f4;
+  margin-top: 2vh;
+}
+
+
+// #nav-container {
+//  display: flex;
+//  justify-content: space-between;
+//  flex-direction: row-reverse;
+//  padding: 20px;
+// }
+
 #nav {
-  padding: 30px;
+  // padding: 30px;
+  position: fixed;
+  right: 1.5vw;
+  top: 3vh;
+  
 
   a {
-    font-weight: bold;
     color: #2c3e50;
+    text-decoration:none;
+    font-family: 'Bebas Neue';
+    font-size: 1.5vw;
+    color:#f4f4f4;
 
     &.router-link-exact-active {
       color: #42b983;
     }
   }
 }
+
+$color-bg: #1e1e1e;
+$color-particle: #fff;
+$spacing: 2560px;
+$time-1: 60s;
+$time-2: 120s;
+$time-3: 180s;
+$time-4: 200s;
+
+/* mixins */
+@function particles($max) {
+  $val: 0px 0px $color-particle;
+  @for $i from 1 through $max {
+    $val: #{$val}, random($spacing)+px random($spacing)+px $color-particle;
+  }
+  @return $val;
+}
+
+@mixin particles($max) {
+  box-shadow: particles($max);
+  border-radius: 50%;
+}
+
+/* Styles */
+
+
+#page-bg, .animation-wrapper {
+//  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+#page-bg {
+ // background: $color-bg;
+ // background-blend-mode: screen;
+//  background-size: cover;
+ // filter: grayscale(100%);
+ z-index: -1;
+}
+
+.particle, .particle:after {
+  background: transparent;
+}
+
+.particle:after {
+  position: absolute;
+  content: "";
+  top: $spacing;
+}
+
+.particle-1 {
+  animation: animParticle $time-1 linear infinite;
+  @include particles(1000);
+  height: 2px;
+  width: 2px;
+}
+
+.particle-1:after {
+  @include particles(900);
+  height: 2px;
+  width: 2px;
+}
+
+.particle-2 {
+  animation: animParticle $time-2 linear infinite;
+  @include particles(800);
+  height: 2px;
+  width: 2px;
+}
+
+.particle-2:after {
+  @include particles(500);
+  height: 3px;
+  width: 3px;
+}
+
+.particle-3:after {
+  @include particles(1100);
+  height: 3px;
+  width: 3px;
+  border-radius: 50%;
+}
+
+.particle-4 {
+  animation: animParticle $time-4 linear infinite;
+  @include particles(1200);
+  height: 1px;
+  width: 1px;
+}
+
+.particle-4:after {
+  @include particles(700);
+  height: 1px;
+  width: 1px;
+}
+
+@keyframes animParticle {
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY($spacing * -1);
+  }
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="moon logo" src="../assets/logo.png"> -->
+<!--   <HelloWorld msg=""/> -->
+    <div class="top-container">
+    <router-link to="/">
+        <img id="logo-frontpage" src="../assets/logo.png" alt="logo">
+    </router-link>
+    
+      <a id="port-title">Johanna Lakasz portfolio</a>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<style lang="scss">
+  .top-container {
+    display: flex;
+    margin-top: 2vh;
+    margin-left: 2vw;
+    margin-right: 2vw;
+    margin-top: 1.5vh;
+    
+    .logo-frontpage {
+      margin-top: 2vh;
+      height: 0.7vh;
+      width: 0.7vh;
+    }
+  }
+
+  @media only screen and (min-width: 1400px){
+  #logo-frontpage{
+    height:3vh;
+    width: 3vh;
+    margin-left: 2vw;
+    margin-right: 2vw;
+    margin-top: 2vh;
   }
 }
+
+
+
+</style>
+
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//  name: 'Home',
+//  components: {
+//   HelloWorld
+//  }
+// }
 </script>
