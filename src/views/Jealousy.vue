df9f3ffd80e62b55a4b19cc1b9bdd2693eb6d787
johannalakasz <johannalakasz@gmail.com>
2021-05-06 02:12:46 +0200

    logo res

--- src/views/Jealousy.vue
+++ src/views/Jealousy.vue
@@ -9,15 +9,26 @@
 
 
     <h2>Project description</h2>
-    <div class='desc'>What initially was a 1st semester color theory assignment based on the theme of jealousy and specifically the Jealousy music video by the Korean band, Monsta X, has eventually turned into my first all-rounder fashion project. I’ve been interested in making my own clothes for a few years now, but only gotten seriously immersed in the fashion industry, the process of fashion design and sewing very recently. <br>
+    <div class='cors-desc'>What initially was a 1st semester color theory assignment based on the theme of jealousy and specifically the Jealousy music video by the Korean band, Monsta X, has eventually turned into my first all-rounder fashion project. I’ve been interested in making my own clothes for a few years now, but only gotten seriously immersed in the fashion industry, the process of fashion design and sewing very recently. <br>
     <br>This project focuses on the creation of a smart blouse, inspired by stage outfits previously worn by Monsta X and on a combination of feminine and masculine silhouettes. 
     </div>
     <div id="cr-container">
         <img id="corset-research" src="../assets/corset/corset-research-small.png" alt="corset research">
     </div>
+    <h3>Pattern</h3>
+    <div class='cors-desc'>What initially was a 1st semester color theory assignment based on the theme of jealousy and specifically the Jealousy music video by the Korean band, Monsta X, has eventually turned into my first all-rounder fashion project. I’ve been interested in making my own clothes for a few years now, but only gotten seriously immersed in the fashion industry, the process of fashion design and sewing very recently. <br>
+    </div>
+    <div id="pattern-container">
+            <img id="pattern" src="../assets/corset/pattern.png" alt="pattern">
+    </div>
     <h3>Prototype</h3>
+    <div class='cors-desc'>What initially was a 1st semester color theory assignment based on the theme of jealousy and specifically the Jealousy music video by the Korean band, Monsta X, has eventually turned into my first all-rounder fashion project. I’ve been interested in making my own clothes for a few years now, but only gotten seriously immersed in the fashion industry, the process of fashion design and sewing very recently. <br>
+    </div>
     <div id="corset-container">
-            <img id="corset1" src="../assets/corset/corset1.jpg" alt="corset">
+            <img id="corset1" src="../assets/corset/corset1.jpg" alt="corset result pic">
+            <img id="corset2" src="../assets/corset/corset2.jpg" alt="corset result pic">
+            <img id="corset3" src="../assets/corset/corset3.jpg" alt="corset result pic">
+            <img id="corset4" src="../assets/corset/corset4.jpg" alt="corset result pic">
     </div>
 
 
@@ -32,15 +43,81 @@
       width: 75vw;
       margin-left: 5vw;
       margin-top: 7vh;
-      margin-bottom: 10vh;
+      margin-bottom: 6vh;
       z-index: 1;
     }
   }
 
-  #corset-container {
+  #pattern-container {
     display: flex;
+    margin-left: 5vw;
+    margin-bottom: 6vh;
+    #pattern {
+      width: 70vw;
+      margin-top: 5vh;
+      @media only screen and (min-width: 1000px) {
+      width: 40vw;
+      margin-top: 5vh;
+      }
+    }
+  }
+
+  .cors-desc {
+        margin-left: 5vw;
+        width: 85vw;
+        text-align: justify;
+        color: #a8a8a8;
+        @media only screen and (min-width: 1000px) {
+        margin-left: 5vw;
+        width: 77vw;
+        text-align: justify;
+        color: #a8a8a8;  
+        }
+     }
+
+
+    #corset-container {
+    display: flex;
+    margin-left: 5vw;
+    flex-wrap: wrap;
+    margin-bottom: 5vh;
+    justify-content: space-between;
+    margin-right: 15vw;
+    margin-top: 4vh;
     #corset1 {
-      width: 20%;
+      height: 52vh;
+      z-index: 20;
+      margin-bottom: 4vh;
+      @media only screen and (min-width: 1000px) {
+      height: 50vh;
+      z-index: 20;  
+      }
+    }
+     #corset2 {
+      height: 58vh;
+      z-index: 20;
+      margin-bottom: 4vh;
+      @media only screen and (min-width: 1000px) {
+      height: 50vh;
+      z-index: 20;  
+      }
+    }
+     #corset3 {
+      height: 52vh;
+      z-index: 20;
+      margin-bottom: 4vh;
+      @media only screen and (min-width: 1000px) {
+      height: 50vh;
+      z-index: 20;  
+      }
+    }
+     #corset4 {
+      height: 58vh;
+      z-index: 20;
+      @media only screen and (min-width: 1000px) {
+      height: 50vh;
+      z-index: 20;  
+      }
     }
   }
 </style>
--- src/views/Logo.vue
+++ src/views/Logo.vue
@@ -15,11 +15,13 @@
         <img id="logo-research" src="../assets/logo/logo-res.png" alt="logo research">
 </div>
 <h3>Sketching</h3>
+<div class='logo-desc'>Idea house is a student start-up at EASV responsible for helping students starting their own company. Students can go on an internship there focusing on entrepreneurship. The internship lasts 10-12 weeks and consists of a structured program with a range of activities that help students build their business foundation.</div>
 <div id="sketch-container">
         <img id="sketch1" src="../assets/logo/sketch1.jpg" alt="logo sketch">
         <img id="sketch2" src="../assets/logo/sketch2.jpg" alt="logo sketch">
 </div>
 <h3>Prototype</h3>
+<div class='logo-desc'>Idea house is a student start-up at EASV responsible for helping students starting their own company. Students can go on an internship there focusing on entrepreneurship. The internship lasts 10-12 weeks and consists of a structured program with a range of activities that help students build their business foundation.</div>
 <div id="logopro-container">
         <img id="logopro1" src="../assets/logo/idea_final.png" alt="final logo">
         <img id="logopro2" src="../assets/logo/idea-proto.png" alt="logo">
@@ -44,35 +46,74 @@
       display: flex;
       margin-left: 5vw;
       margin-bottom: 7vh;
+      justify-content: space-between;
+      margin-right: 8vw;
+      margin-top: 4vh;
+   //   flex-wrap: wrap;
+   @media only screen and (min-width: 1000px) {
+    display: flex;
+      margin-left: 5vw;
+      margin-bottom: 7vh;
+      justify-content: left;
+   }
       #sketch1 {
-          height: 70vh;
+          height: 17vh;
           margin-right: 1vw;
           z-index: 3;
+          @media only screen and (min-width: 1000px) {
+          height: 70vh;
+          margin-right: 1vw;
+          z-index: 3;    
+          }
       }
       #sketch2 {
-          height: 60vh;
+        height: 17vh;
+        z-index: 4;
+          @media only screen and (min-width: 1000px)  {
+            height: 60vh;
             z-index: 4;
+          }
       }
   }
 
   #logopro-container {
       display: flex;
       margin-left: 5vw;
+      flex-wrap: wrap;
+      margin-top: 4vh;
       #logopro1 {
-          height: 50vh;
+          height: 40vh;
           margin-right: 1vw;
           z-index: 4;
+          @media only screen and (min-width: 1000px) {
+          height: 50vh;
+          margin-right: 1vw;
+          z-index: 4;    
+          }
       }
       #logopro2 {
           height: 40vh;
           z-index: 4;
+          margin-top: 3vh;
+          margin-bottom: 5vh;
+          @media only screen and (min-width: 1000px) {
+          height: 40vh;
+          z-index: 4; 
+          margin-top: 0;   
+          }
       }
   }
 
       .logo-desc {
         margin-left: 5vw;
+        margin-right: 8vw;
+        text-align: justify;
+        color: #a8a8a8;
+        @media only screen and (min-width: 1000px) {
+        margin-left: 5vw;
         margin-right: 15vw;
         text-align: justify;
         color: #a8a8a8;
+        }
      }
 </style>
